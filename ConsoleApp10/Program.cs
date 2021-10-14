ad3087880520e5176420f406a40fed4fdc4ce340
Kiberio <dhiroshi3@gmail.com>
2021-10-13 21:23:02 -0300

    Comentarios

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -7,7 +7,9 @@ namespace ConsoleApp10.Classes
     {
         static void Main(string[] args)
         {
+            //Inserindo dados da conta bancaria
             ContaJuridica contaJuridicaconta = new ContaJuridica(8010,"LaraCroft", 100.0, 500.0);
+            //Mostrando o saldo da conta
             Console.WriteLine(value:contaJuridicaconta.Saldo);
             Console.ReadKey();
         }
